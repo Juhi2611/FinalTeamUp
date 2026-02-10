9681ff9b5f695f02a9e876a4dc246f641cde472b
Juhi <vanjarajuhi2611@gmail.com>
2026-02-10 17:58:54 +0530

    Create DemoPage.tsx

---
+++ src/pages/DemoPage.tsx
@@ -0,0 +1,11 @@
+import DemoDashboard from "@/components/demo/DemoDashboard";
+
+const DemoPage = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <DemoDashboard />
+    </div>
+  );
+};
+
+export default DemoPage;
